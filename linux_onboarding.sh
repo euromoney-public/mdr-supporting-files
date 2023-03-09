829af9467bfd3e166900cc940212186d69f8f6ca
Rob Emmerson <rob.emmerson@gmail.com>
2023-03-09 23:28:31 +0000

    Added dummy Linux installer

---
+++ linux_onboarding.sh
@@ -0,0 +1,8 @@
+# Alienvault On-boarding Script
+# Written by Rob Emmerson
+
+red_pre="\033[31m"
+red_post="\033[0m"
+
+echo -e $red_pre"Error: Alienvault On-boarding is currently disabled!"$red_post
+exit 1
