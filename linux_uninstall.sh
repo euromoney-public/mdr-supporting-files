a62e02f6d595131b13e85413f2fbf399e8f0bbfb
Rob Emmerson <rob.emmerson@gmail.com>
2025-04-29 17:30:13 +0100

    Fixed uninstall script

--- linux_uninstall.sh
+++ linux_uninstall.sh
@@ -15,27 +15,37 @@ if [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
 fi
 
 if [ -f "/usr/bin/alienvault-agent.sh" ]; then
+    echo "Perform an uninstall using the Alienvault script..."
     $sudo_cmd alienvault-agent.sh uninstall
 
+    echo "[OS SPECIFIC] Purge Alienvault as it doesn't cleanly uninstall..."
     $sudo_cmd apt-get remove --purge -y alienvault-agent
 
+    echo "Attempting to stop and disable osqueryd incase the script didn't..."
     $sudo_cmd systemctl stop osqueryd
     $sudo_cmd systemctl disable osqueryd
 
     if [ -f "/lib/systemd/system/osqueryd.service" ]; then
+        echo "Removing /lib/systemd/system/osqueryd.service..."
         $sudo_cmd rm -f /lib/systemd/system/osqueryd.service
     fi
 
     if [ -f "/usr/lib/systemd/system/osqueryd.service" ]; then
+        echo "Removing /usr/lib/systemd/system/osqueryd.service..."
         $sudo_cmd rm -f /usr/lib/systemd/system/osqueryd.service
     fi
 
     if [ -f "/etc/init.d/osqueryd" ]; then
+        echo "Removing /etc/init.d/osqueryd..."
         $sudo_cmd rm -f /etc/init.d/osqueryd
     fi
 
+    echo "Running 'systemctl daemon-reload'..."
     $sudo_cmd systemctl daemon-reload
+    
+    echo "Running 'systemctl reset-failed'..."
     $sudo_cmd systemctl reset-failed
 
+    echo "Tidying up osqueryd files..."
     $sudo_cmd rm -rf /etc/osquery /var/osquery
 fi
