45ef418a2e2c1c2d02fa241549098bbdcad897d9
Rob Emmerson <rob.emmerson@gmail.com>
2025-09-05 09:43:02 +0100

    Added additional duplicate UUIDs

--- linux_onboarding.sh
+++ linux_onboarding.sh
@@ -24,7 +24,7 @@ if [ -f "$FLAGCHECK" ]; then
 
     # Check if HOST_ID matches any of the known duplicated IDs
     case "$HOST_ID" in
-        00000000-0c8f-4f3d-ba95-86a0afb9d9df|00000000-1261-4734-ba88-6e761309a0c7|00000000-0a70-48d5-baff-7ccbbecbe0f9|00000000-c4ba-48ac-91f5-0be09cebd1e8|00000000-fa2a-4cb0-9b88-fe979a3fa6cc)
+        00000000-0c8f-4f3d-ba95-86a0afb9d9df|00000000-1261-4734-ba88-6e761309a0c7|00000000-0a70-48d5-baff-7ccbbecbe0f9|00000000-c4ba-48ac-91f5-0be09cebd1e8|00000000-fa2a-4cb0-9b88-fe979a3fa6cc|00000000-5ca6-4f0a-b10a-9bd3e2766251|00000000-ea5d-4ee7-a0c8-181306421ea2|00000000-c588-47e3-bdb2-856a21f8ab6b|00000000-8333-4120-a16d-a2e587417406)
             echo ""
             echo "Re-install required, tidying up..."
             $sudo_cmd alienvault-agent.sh uninstall
